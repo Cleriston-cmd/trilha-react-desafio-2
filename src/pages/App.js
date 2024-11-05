4adf2aaf432694d9680143c752b77cb4bc2d124c
Cleriston-cmd <c.andrade6533@gmail.com>
2024-11-05 10:35:13 -0300

    Atualizações e correções no projeto

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,49 @@
 
+import { useState } from "react";
+import logoGit from "../assets/logoGit.png";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import ItemRepo from "../components/ItemRepo";
+import { api } from "../services/api";
+
+import { Container } from './styles';
+
 function App() {
+  
+  const [currentRepo, setCurrentRepo] = useState('');
+  const [repos, setRepos] = useState([]);
+
+
+  const handleSearchRepo = async () => {
+
+    const {data} = await api.get(`repos/${currentRepo}`)
+
+    if(data.id){
+      
+      const isExist = repos.find(repo => repo.id === data.id);
+
+      if(!isExist){
+        setRepos(prev => [...prev, data]);
+        setCurrentRepo('')
+      return
+      }
+    }
+    alert("Repositório não encontrado")
+  }
+
+  const handleRemoveRepo = (id) => {
+    console.log('Removendo registro', id);
+
+    // utilizar filter.
+  }
+
   return (
-    <div className="App">
-      Olá
-    </div>
+    <Container>
+      <img src={logoGit} width={72} height={72} alt="github logo" />
+      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
+      <Button onClick={handleSearchRepo}/>
+      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+    </Container>
   );
 }
 
